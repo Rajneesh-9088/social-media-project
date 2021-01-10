781de25a38457284dbc62d97229b47940a50dff7
proraj9088 <rp8735685@gmail.com>
2021-01-11 02:03:50 +0530

    show all posts on home page
    Signed-off-by: proraj9088 <rp8735685@gmail.com>

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,6 +1,8 @@
 $(() => {
     $('#navbar').load('/components/navbar.html', loginIfNeeded)
     $('#footer').load('/components/footer.html')
+    $('#content').load('/components/all-posts.html', loadPost)
+
     // loginIfNeeded()
 
 })
@@ -9,7 +11,7 @@ function loginIfNeeded() {
 
     if (!currentUser) {
         $.post('/api/users', {}, (user) => {
-            
+
             if (user) {
                 console.log('registered as ', user.username)
                 window.localStorage.user = JSON.stringify(user)
@@ -18,9 +20,34 @@ function loginIfNeeded() {
                 $('#nav-username').text(currentUser.username)
             }
         })
-    }  
-     else {
+    }
+    else {
         console.log('resuming session as', currentUser.username)
         $('#nav-username').text(currentUser.username)
- }
+    }
+}
+
+function loadPost() {
+    $.get('/api/posts', (posts) => {
+        for (let p of posts) {
+            $('#posts-container').append(
+                $(`
+                <div class="col-4 ">
+                <div class="card m-2 " style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${p.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+                    <p class="card-text">${p.body.substr(0,200)}
+                    <a href="#" >...readMore</a>
+                    </p>   
+                    <a href="#" class="card-link">Comments</a>
+                    <a href="#" class="card-link">Like</a>
+                </div>
+                </div>
+                </div>
+               
+               `)
+            )
+        }
+    })
 }
